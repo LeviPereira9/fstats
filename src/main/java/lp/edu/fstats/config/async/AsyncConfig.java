c9fbfe8836288b1316fbd13b0143cd65e21a64f5
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2025-11-14 19:28:02 -0300

    feat<async>: configuração de Async, para os emails.

---
+++ src/main/java/lp/edu/fstats/config/async/AsyncConfig.java
@@ -0,0 +1,29 @@
+package lp.edu.fstats.config.async;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "emailThread")
+    public TaskExecutor emailTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("EmailAsync-");
+
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.DiscardPolicy());
+        executor.initialize();
+
+        return executor;
+    }
+
+}
