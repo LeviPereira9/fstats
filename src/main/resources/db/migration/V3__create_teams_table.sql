bc0766a30bf74449750f75baa5009325b93d394d
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2025-11-30 17:37:08 -0300

    fix: soft delete

--- src/main/resources/db/migration/V3__create_teams_table.sql
+++ src/main/resources/db/migration/V3__create_teams_table.sql
@@ -2,9 +2,11 @@ CREATE TABLE IF NOT EXISTS TB_Time(
     ID_Time             BIGINT          AUTO_INCREMENT,
     ID_ExternoTime      BIGINT          NOT NULL,
     Nm_Time             VARCHAR(255)    NOT NULL,
-    Nm_TimeAbreviado    VARCHAR(255)     NOT NULL,
-    Cd_SiglaTime        VARCHAR(255)      NOT NULL,
+    Nm_TimeAbreviado    VARCHAR(255)    NOT NULL,
+    Cd_SiglaTime        VARCHAR(255)    NOT NULL,
     Ur_Emblema          VARCHAR(255),
+
+    St_Ativo            BOOLEAN         DEFAULT TRUE,
 
     PRIMARY KEY (ID_Time),
     UNIQUE (ID_ExternoTime)
