5b057855164e16bc4910140afe2bb3ab585ab54e
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2025-11-30 17:38:15 -0300

    refactor: agora o sync atualiza se já existir.

--- src/main/java/lp/edu/fstats/integration/service/football/FootballSyncService.java
+++ src/main/java/lp/edu/fstats/integration/service/football/FootballSyncService.java
@@ -2,10 +2,13 @@ package lp.edu.fstats.integration.service.football;
 
 import lombok.RequiredArgsConstructor;
 import lp.edu.fstats.integration.client.FootballApiClient;
-import lp.edu.fstats.integration.dto.matches.MatchExternalResponse;
+import lp.edu.fstats.integration.dto.matches.match.MatchExternalResponse;
 import lp.edu.fstats.integration.dto.matches.MatchesExternalResponse;
+import lp.edu.fstats.integration.dto.matches.match.TeamExternalResponse;
+import lp.edu.fstats.model.competition.Competition;
 import lp.edu.fstats.model.match.Match;
 import lp.edu.fstats.model.team.Team;
+import lp.edu.fstats.repository.competition.CompetitionRepository;
 import lp.edu.fstats.repository.match.MatchRepository;
 import lp.edu.fstats.repository.team.TeamRepository;
 import lp.edu.fstats.service.match.MatchService;
@@ -25,21 +28,31 @@ public class FootballSyncService {
     private final TeamService teamService;
     private final TeamRepository teamRepository;
     private final MatchRepository matchRepository;
+    private final CompetitionRepository competitionRepository;
 
 
     public void Manage(){
         MatchesExternalResponse externalMatches = footballApiClient.getCurrentMatches();
 
-        List<Integer> externalTeamsIds = externalMatches.getTeamsExternalIds();
+        Long externalCompetitionId = externalMatches.getCompetitionExternalId();
 
-        List<Integer> externalMatchIds = externalMatches.getMatchesExternalIds();
+        Competition competition = competitionRepository.findByExternalId(externalCompetitionId)
+                .orElse(externalMatches.competitionToModel());
+
+        if(competition.getId() == null){
+            competition = competitionRepository.save(competition);
+        }
+
+        List<Long> externalTeamsIds = externalMatches.getTeamsExternalIds();
+
+        List<Long> externalMatchIds = externalMatches.getMatchesExternalIds();
 
         // Oq vai acontecer?
         // Verificamos se os times/partidas/pontuações existem.
         // Se existe, a gente pega pra atualizar
         // Se não existe, vai ser criado.
-        Map<Integer, Team> mapTeams = teamService.findAllByExternalId(externalTeamsIds);
-        Map<Integer, Match> mapMatches = matchService.findAllByExternalId(externalMatchIds);
+        Map<Long, Team> mapTeams = teamService.findAllByExternalId(externalTeamsIds);
+        Map<Long, Match> mapMatches = matchService.findAllByExternalId(externalMatchIds);
 
         List<MatchExternalResponse> matches = externalMatches.matches();
 
@@ -47,17 +60,17 @@ public class FootballSyncService {
         List<Match> matchesToSave = new ArrayList<>();
 
         for(MatchExternalResponse match : matches){
-            Match currentMatch = mapMatches.getOrDefault(
-                    match.id(),
-                    match.toModel());
+            Match currentMatch = this.getMatch(
+                    mapMatches.get(match.id()),
+                    match);
 
-            Team homeTeam = mapTeams.getOrDefault(
-                    match.getHomeTeamExternalId(),
-                    match.homeTeamToModel());
+            Team homeTeam = this.getTeam(
+                    mapTeams.get(match.getHomeTeamExternalId()),
+                    match.homeTeam());
 
-            Team awayTeam = mapTeams.getOrDefault(
-                    match.getAwayTeamExternalId(),
-                    match.awayTeamToModel());
+            Team awayTeam = this.getTeam(
+                    mapTeams.get(match.getAwayTeamExternalId()),
+                    match.awayTeam());
 
             if(currentMatch.getHomeTeam() == null){
                 currentMatch.setHomeTeam(homeTeam);
@@ -67,12 +80,29 @@ public class FootballSyncService {
                 currentMatch.setAwayTeam(awayTeam);
             }
 
+            if(currentMatch.getCompetition() == null){
+                currentMatch.setCompetition(competition);
+            }
+
             teamsToSave.addAll(List.of(homeTeam, awayTeam));
             matchesToSave.add(currentMatch);
         }
 
         teamRepository.saveAll(teamsToSave);
         matchRepository.saveAll(matchesToSave);
+    }
+
+    private Team getTeam(Team currentTeam, TeamExternalResponse externalTeam) {
+        if(currentTeam == null) return externalTeam.toModel();
+
+        return externalTeam.update(currentTeam);
+    }
+
+    private Match getMatch(Match currentMatch, MatchExternalResponse externalMatch) {
+
+        if(currentMatch == null) return externalMatch.toModel();
+
+        return externalMatch.update(currentMatch);
     }
 
 }
