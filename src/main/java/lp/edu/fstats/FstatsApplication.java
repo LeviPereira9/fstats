cbd664803554b284f14f85bd4825071992b66d5a
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2025-11-30 15:24:34 -0300

    feat: toda vez que inicia a aplicacao, synca.

--- src/main/java/lp/edu/fstats/FstatsApplication.java
+++ src/main/java/lp/edu/fstats/FstatsApplication.java
@@ -1,13 +1,20 @@
 package lp.edu.fstats;
 
+import lp.edu.fstats.integration.service.football.FootballSyncService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class FstatsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FstatsApplication.class, args);
+        ApplicationContext context = SpringApplication.run(FstatsApplication.class, args);
+
+        FootballSyncService syncService = context.getBean(FootballSyncService.class);
+
+        syncService.Manage();
+
     }
 
 }
