8ee3aefcdac658d91f5df316a6163a7487864c74
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2025-11-30 15:19:04 -0300

    feat<schemas>: tabelas de time e partida.

--- src/main/resources/db/migration/V3__create_teams_table.sql
+++ src/main/resources/db/migration/V3__create_teams_table.sql
@@ -1,7 +1,11 @@
 CREATE TABLE IF NOT EXISTS TB_Time(
-    ID_Time BIGINT AUTO_INCREMENT,
-    Nm_Time VARCHAR(255) NOT NULL,
-    Ur_Emblema VARCHAR(255),
+    ID_Time             BIGINT          AUTO_INCREMENT,
+    ID_ExternoTime      BIGINT          NOT NULL,
+    Nm_Time             VARCHAR(255)    NOT NULL,
+    Nm_TimeAbreviado    VARCHAR(255)     NOT NULL,
+    Cd_SiglaTime        VARCHAR(255)      NOT NULL,
+    Ur_Emblema          VARCHAR(255),
 
-    PRIMARY KEY (ID_Time)
+    PRIMARY KEY (ID_Time),
+    UNIQUE (ID_ExternoTime)
 )engine=InnoDB default charset=utf8mb4;
---
+++ src/main/resources/db/migration/V4__create_match_table.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS TB_Partida(
+    ID_Partida              BIGINT AUTO_INCREMENT,
+    ID_ExternoPartida       BIGINT NOT NULL,
+    Dt_Partida              DATETIME,
+    St_Partida              VARCHAR(50) NOT NULL,
+    Nr_Rodada               INTEGER,
+    Ds_Estagio              VARCHAR(50),
+    ID_TimeCasa             BIGINT NOT NULL,
+    ID_TimeVisitante        BIGINT NOT NULL,
+    Ds_Ganhador             VARCHAR(255),
+    Qt_GolTimeCasa          INTEGER NOT NULL,
+    Qt_GolTimeVisitante     INTEGER NOT NULL,
+
+    PRIMARY KEY (ID_Partida),
+    UNIQUE (ID_Partida),
+    FOREIGN KEY (ID_TimeCasa)
+        REFERENCES TB_Time(ID_Time),
+    FOREIGN KEY (ID_TimeVisitante)
+        REFERENCES TB_Time(ID_Time)
+)engine=InnoDB default charset=utf8mb4;
