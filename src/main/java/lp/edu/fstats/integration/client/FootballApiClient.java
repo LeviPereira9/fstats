bd0c07d304ba246462745593306c2cc83eb94fd6
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2025-12-03 19:13:33 -0300

    feat: nova endpoint para competições.

--- src/main/java/lp/edu/fstats/integration/client/FootballApiClient.java
+++ src/main/java/lp/edu/fstats/integration/client/FootballApiClient.java
@@ -1,6 +1,7 @@
 package lp.edu.fstats.integration.client;
 
 import lombok.RequiredArgsConstructor;
+import lp.edu.fstats.integration.dto.competition.CompetitionExternalResponse;
 import lp.edu.fstats.integration.dto.matches.MatchesExternalResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
@@ -11,16 +12,26 @@ public class FootballApiClient {
 
     private final RestClient restClient;
 
-    public MatchesExternalResponse getCurrentMatches(String season, Integer matchday){
+    public MatchesExternalResponse getCurrentMatches(String code, String season, Integer matchday){
         return restClient.get()
                 .uri(uriBuilder ->
                         uriBuilder
-                                .path("/competitions/PL/matches")
+                                .path("/competitions/"+code+"/matches")
                                 .queryParam("season", season)
                                 .queryParam("matchday", matchday)
                                 .build())
                 .retrieve()
                 .body(MatchesExternalResponse.class);
+    }
+
+    public CompetitionExternalResponse getCurrentCompetition(String code){
+        return restClient.get()
+                .uri(uriBuilder ->
+                        uriBuilder
+                                .path("/competitions/"+code)
+                                .build())
+                .retrieve()
+                .body(CompetitionExternalResponse.class);
     }
 
 }
