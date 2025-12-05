140a54269ab298bab82e4713ad160d105ad0c087
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2025-12-04 21:00:04 -0300

    feat<repository>: competição

---
+++ src/main/java/lp/edu/fstats/repository/standings/StandingsRepository.java
@@ -0,0 +1,11 @@
+package lp.edu.fstats.repository.standings;
+
+import lp.edu.fstats.model.standings.Standings;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface StandingsRepository extends JpaRepository<Standings, Long> {
+
+    List<Standings> findAllByCompetition_Id(Long id);
+}
