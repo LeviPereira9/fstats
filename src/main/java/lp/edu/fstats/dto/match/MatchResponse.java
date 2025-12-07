5d7cfcd3676aca9cee24d79a4cd3d1b9e042368c
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2025-12-06 22:06:15 -0300

    feat<dto>: match response vem com as probability.

--- src/main/java/lp/edu/fstats/dto/match/MatchResponse.java
+++ src/main/java/lp/edu/fstats/dto/match/MatchResponse.java
@@ -1,5 +1,6 @@
 package lp.edu.fstats.dto.match;
 
+import lp.edu.fstats.dto.probability.ProbabilityResponse;
 import lp.edu.fstats.model.match.Match;
 import lp.edu.fstats.model.team.Team;
 
@@ -9,25 +10,25 @@ public record MatchResponse(
         //Long id,
         TeamResponse home,
         TeamResponse away,
+        ProbabilityResponse probability,
         LocalDateTime date
 ) {
 
-    public MatchResponse(Match match) {
-
+    public MatchResponse(Match source) {
 
         this(
                 //match.getId(),
                 new TeamResponse(
-                        match.getHomeTeam().getId(),
-                        match.getHomeTeam().getShortName(),
-                        match.getHomeGoals(),
-                        match.getHomeTeam().getCrest()),
+                        source.getHomeTeam(),
+                        source.getHomeGoals(),
+                        source.getHomeTeam().getCrest()),
                 new TeamResponse(
-                        match.getAwayTeam().getId(),
-                        match.getAwayTeam().getShortName(),
-                        match.getAwayGoals(),
-                        match.getAwayTeam().getCrest()),
-                match.getUtcDate()
+                        source.getAwayTeam(),
+                        source.getAwayGoals(),
+                        source.getAwayTeam().getCrest()),
+                (source.getProbability() == null ? null :
+                        new ProbabilityResponse(source.getProbability())),
+                source.getUtcDate()
         );
     }
 }
