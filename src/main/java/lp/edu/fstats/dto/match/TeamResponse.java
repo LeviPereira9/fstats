c6c5b5fb38aa7c034e218f2f79decd410b4ac053
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2025-12-06 22:09:57 -0300

    feat: um bom constructor.

--- src/main/java/lp/edu/fstats/dto/match/TeamResponse.java
+++ src/main/java/lp/edu/fstats/dto/match/TeamResponse.java
@@ -1,8 +1,19 @@
 package lp.edu.fstats.dto.match;
 
+import lp.edu.fstats.model.team.Team;
+
 public record TeamResponse(
         Long id,
         String name,
         Integer goals,
         String emblem
-) {}
+) {
+    public TeamResponse(Team source, Integer goals, String emblem){
+        this(
+                source.getId(),
+                source.getShortName(),
+                goals,
+                emblem
+        );
+    }
+}
