4f3dc94b2ed2af84cdf00e12395312a71de8df3f
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2025-12-07 09:14:04 -0300

    refactor<controller>: agr tudo da competicao fica no mesmo lugar

--- src/main/java/lp/edu/fstats/controller/competition/CompetitionController.java
+++ src/main/java/lp/edu/fstats/controller/competition/CompetitionController.java
@@ -1,11 +1,15 @@
 package lp.edu.fstats.controller.competition;
 
 import lombok.RequiredArgsConstructor;
+import lp.edu.fstats.dto.averages.AveragesResponse;
 import lp.edu.fstats.dto.competition.CompetitionResponse;
 import lp.edu.fstats.dto.match.MatchesResponse;
+import lp.edu.fstats.dto.standings.StandingsResponse;
 import lp.edu.fstats.response.normal.Response;
+import lp.edu.fstats.service.averages.AveragesService;
 import lp.edu.fstats.service.competition.CompetitionService;
 import lp.edu.fstats.service.match.MatchService;
+import lp.edu.fstats.service.standings.StandingsService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +20,8 @@ import org.springframework.web.bind.annotation.*;
 public class CompetitionController {
     private final CompetitionService competitionService;
     private final MatchService matchService;
+    private final StandingsService standingsService;
+    private final AveragesService averagesService;
 
     @GetMapping("/{code}")
     public ResponseEntity<Response<CompetitionResponse>> getCompetition(
@@ -34,12 +40,12 @@ public class CompetitionController {
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/{code}/matches")
+    @GetMapping("/{competitionId}/matches")
     public ResponseEntity<Response<MatchesResponse>> getMatches(
-            @PathVariable String code,
+            @PathVariable Long competitionId,
             @RequestParam(defaultValue = "1") Integer matchday
     ){
-        MatchesResponse data = matchService.getMatches(code, matchday);
+        MatchesResponse data = matchService.getMatches(competitionId, matchday);
 
         Response<MatchesResponse> response = Response.<MatchesResponse>builder()
                 .operation("Competition.Matches.GetMatches")
@@ -50,4 +56,38 @@ public class CompetitionController {
 
         return ResponseEntity.ok(response);
     }
+
+    @GetMapping("/{competitionId}/averages")
+    public ResponseEntity<Response<AveragesResponse>> getAverages(
+            @PathVariable Long competitionId
+    ){
+        AveragesResponse data = averagesService.findAllByCompetition(competitionId);
+
+        Response<AveragesResponse> response = Response.<AveragesResponse>builder()
+                .operation("Competition.Averages.GetAverages")
+                .code(HttpStatus.OK.value())
+                .data(data)
+                .message("Médias encontradas com sucesso.")
+                .build();
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{competitionId}/standings")
+    public ResponseEntity<Response<StandingsResponse>> getStandings(
+            @PathVariable Long competitionId
+    ){
+        StandingsResponse data = standingsService.getStandings(competitionId);
+
+        Response<StandingsResponse> response = Response.<StandingsResponse>builder()
+                .operation("")
+                .code(HttpStatus.OK.value())
+                .message("Classificações encontradas com sucesso.")
+                .data(data)
+                .build();
+
+        return ResponseEntity.ok(response);
+    }
+
+
 }
