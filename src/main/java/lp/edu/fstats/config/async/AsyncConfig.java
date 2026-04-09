6828d60a35c9d53961f51e662f01204bc48d2cef
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2026-04-09 18:10:56 -0300

    feat: config de novas threads

--- src/main/java/lp/edu/fstats/config/async/AsyncConfig.java
+++ src/main/java/lp/edu/fstats/config/async/AsyncConfig.java
@@ -3,12 +3,14 @@ package lp.edu.fstats.config.async;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.ThreadPoolExecutor;
 
 @Configuration
+@EnableRetry
 @EnableAsync
 public class AsyncConfig {
 
@@ -25,5 +27,34 @@ public class AsyncConfig {
 
         return executor;
     }
+
+    @Bean(name = "competitionThread")
+    public TaskExecutor competitionTaskExecutor(){
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(5);
+        executor.setThreadNamePrefix("CompetitionAsync-");
+
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.DiscardPolicy());
+        executor.initialize();
+
+        return executor;
+    }
+
+    @Bean(name = "competitionsThread")
+    public TaskExecutor competitionsTaskExecutor(){
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(1);
+        executor.setMaxPoolSize(1);
+        executor.setQueueCapacity(0);
+        executor.setThreadNamePrefix("CompetitionsAsync-");
+
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.DiscardPolicy());
+        executor.initialize();
+
+        return executor;
+    }
+
 
 }
