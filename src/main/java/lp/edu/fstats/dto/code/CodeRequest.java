f82844a44eeb893af4582662b2527345aa2b17e7
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2026-06-18 20:12:15 -0300

    feat: campo de name na request de adicionar campeonatos

--- src/main/java/lp/edu/fstats/dto/code/CodeRequest.java
+++ src/main/java/lp/edu/fstats/dto/code/CodeRequest.java
@@ -3,12 +3,14 @@ package lp.edu.fstats.dto.code;
 import lp.edu.fstats.model.code.Code;
 
 public record CodeRequest(
-        String code
+        String code,
+        String name
 ){
 
     public Code toModel(){
         Code code = new Code();
         code.setCode(this.code);
+        code.setName(this.name);
         return code;
     }
 
