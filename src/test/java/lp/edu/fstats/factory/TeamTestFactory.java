cbe14723c2786a5fd569920dd631683068056856
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2026-06-29 17:01:40 -0300

    feat: build dos times para teste

---
+++ src/test/java/lp/edu/fstats/factory/TeamTestFactory.java
@@ -0,0 +1,19 @@
+package lp.edu.fstats.factory;
+
+import lp.edu.fstats.model.team.Team;
+
+public class TeamTestFactory {
+
+    public static Team buildTeam(Long id, Long externalId, String name){
+        Team team = new Team();
+        team.setId(id);
+        team.setExternalId(externalId);
+        team.setName(name);
+        team.setShortName(name);
+        team.setTla(name.substring(0, 3).toUpperCase());
+        team.setCrest("crest.png");
+
+        return team;
+    }
+
+}
