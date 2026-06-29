bac42c17ac1d64914ffa83a80a175d5a3c60255d
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2026-06-29 17:02:04 -0300

    feat: build dos códigos das competições para teste

---
+++ src/test/java/lp/edu/fstats/factory/CodeTestFactory.java
@@ -0,0 +1,15 @@
+package lp.edu.fstats.factory;
+
+import lp.edu.fstats.model.code.Code;
+
+public class CodeTestFactory {
+
+    public static Code buildCode(Integer id, String code, String name){
+        Code c = new Code();
+        c.setId(id);
+        c.setCode(code);
+        c.setName(name);
+
+        return c;
+    }
+}
