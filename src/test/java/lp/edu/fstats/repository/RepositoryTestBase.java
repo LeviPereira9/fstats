abf02daa312a34a2a103ca1a0806cb05d4b8178d
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2026-07-14 15:54:46 -0300

    feat: set up do testcontainer no docker

---
+++ src/test/java/lp/edu/fstats/repository/RepositoryTestBase.java
@@ -0,0 +1,34 @@
+package lp.edu.fstats.repository;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Testcontainers
+public abstract class RepositoryTestBase {
+
+    static final MySQLContainer<?> mysql;
+
+    static {
+        mysql = new MySQLContainer<>("mysql:8.0")
+                .withDatabaseName("fstats_test")
+                .withUsername("test")
+                .withPassword("test");
+
+        mysql.start();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+
+    }
+
+}
