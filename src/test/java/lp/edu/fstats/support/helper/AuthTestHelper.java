2488a5bd402519cff4a675c90dc01bd491ff9c4c
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2026-07-19 20:33:13 -0300

    feat: helper para auth dos testes de IT

---
+++ src/test/java/lp/edu/fstats/support/helper/AuthTestHelper.java
@@ -0,0 +1,57 @@
+package lp.edu.fstats.support.helper;
+
+import io.restassured.http.Cookie;
+import lombok.RequiredArgsConstructor;
+import lp.edu.fstats.model.user.Role;
+import lp.edu.fstats.model.user.User;
+import lp.edu.fstats.repository.user.UserRepository;
+import lp.edu.fstats.security.jwt.service.JwtTokenService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+@RequiredArgsConstructor
+public class AuthTestHelper {
+
+    private final UserRepository userRepository;
+    private final JwtTokenService jwtTokenService;
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public User createUser(String username, String email, String rawPassword, Role role){
+        User user = new User();
+        user.setUsername(username);
+        user.setEmail(email);
+
+        user.setPassword(passwordEncoder.encode(rawPassword));
+
+        user.setDateOfBirth(LocalDate.now().minusYears(20));
+
+        user.setRole(role);
+        user.setVerified(true);
+
+        return userRepository.save(user);
+    }
+
+    public User createDefaultUser(String rawPassword){
+
+        return this.createUser(
+                "user_test",
+                "user_test@test.com",
+                rawPassword,
+                Role.USER
+                );
+
+    }
+
+    public Cookie authCookie(User user){
+        String token = jwtTokenService.generateToken(user);
+
+        return new Cookie.Builder("access_token", token)
+                .setPath("/")
+                .build();
+    }
+
+}
