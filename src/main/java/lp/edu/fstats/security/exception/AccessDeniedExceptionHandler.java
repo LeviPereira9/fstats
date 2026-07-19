c6eb5171774578211a1092b4056712c56d58c124
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2026-07-19 19:56:38 -0300

    feat: handler para exceção de acesso do security

---
+++ src/main/java/lp/edu/fstats/security/exception/AccessDeniedExceptionHandler.java
@@ -0,0 +1,41 @@
+package lp.edu.fstats.security.exception;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lp.edu.fstats.response.normal.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class AccessDeniedExceptionHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper mapper;
+
+    @Override
+    public void handle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+        int code = HttpStatus.FORBIDDEN.value();
+
+        Response<Void> res = Response.<Void>builder()
+                .operation("Error.AccessDenied")
+                .message("Você não têm permissão para acessar este recurso.")
+                .code(code)
+                .build();
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(code);
+        response.getWriter().write(mapper.writeValueAsString(res));
+    }
+}
