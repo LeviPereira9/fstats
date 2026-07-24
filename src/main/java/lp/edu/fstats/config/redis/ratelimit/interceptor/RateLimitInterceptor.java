6f449fd80b5e993abc0146716604b1b64319764e
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2026-07-24 12:50:53 -0300

    refactor: padronizada a mensagem de erro de rate limit

--- src/main/java/lp/edu/fstats/config/redis/ratelimit/interceptor/RateLimitInterceptor.java
+++ src/main/java/lp/edu/fstats/config/redis/ratelimit/interceptor/RateLimitInterceptor.java
@@ -1,19 +1,26 @@
 package lp.edu.fstats.config.redis.ratelimit.interceptor;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lp.edu.fstats.config.redis.ratelimit.service.RateLimitService;
 import lp.edu.fstats.config.redis.ratelimit.snippets.RateLimit;
+import lp.edu.fstats.response.normal.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
 public class RateLimitInterceptor  implements HandlerInterceptor {
 
     private final RateLimitService rateLimitService;
+    private final ObjectMapper objectMapper;
 
     @Override
     public boolean preHandle(
@@ -37,20 +44,27 @@ public class RateLimitInterceptor  implements HandlerInterceptor {
                                 rateLimit.unit())
                 ){
 
-                    response.setStatus(429);
-                    response.setContentType("application/json");
-                    response.getWriter().write(
-                            "{" +
-                                    "\"error\": \"Rate limit exceeded\", " +
-                                    "\"message\": \"Muitas requisições. Tente novamente em alguns minutos.\"" +
-                                    "}"
-                    );
+                    this.generateErrorResponse(response);
                     return false;
                 }
             }
         }
 
         return true;
+    }
+
+    private void generateErrorResponse(HttpServletResponse response) throws IOException {
+        int code = HttpStatus.TOO_MANY_REQUESTS.value();
+
+        Response<Void> res = Response.<Void>builder()
+                .operation("Error.RateLimitExceeded")
+                .code(code)
+                .message("Muitas requisições. Tente novamente em alguns minutos.")
+                .build();
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(code);
+        response.getWriter().write(objectMapper.writeValueAsString(res));
     }
 
 }
