3dd3946d546707b55837c3229a570c45b2c1ba87
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2026-07-27 20:14:45 -0300

    feat: validações de campo (notNull)

--- src/main/java/lp/edu/fstats/dto/favorites/FavoriteAddRequest.java
+++ src/main/java/lp/edu/fstats/dto/favorites/FavoriteAddRequest.java
@@ -1,11 +1,13 @@
 package lp.edu.fstats.dto.favorites;
 
+import jakarta.validation.constraints.NotNull;
 import lp.edu.fstats.model.code.Code;
 import lp.edu.fstats.model.competition.Competition;
 import lp.edu.fstats.model.favorites.Favorite;
 import lp.edu.fstats.model.user.User;
 
 public record FavoriteAddRequest(
+        @NotNull
         Integer competitionId
 ) {
 
--- src/main/java/lp/edu/fstats/dto/favorites/FavoriteRemoveRequest.java
+++ src/main/java/lp/edu/fstats/dto/favorites/FavoriteRemoveRequest.java
@@ -1,5 +1,8 @@
 package lp.edu.fstats.dto.favorites;
 
+import jakarta.validation.constraints.NotNull;
+
 public record FavoriteRemoveRequest(
+        @NotNull
         Long favoriteId
 ) {}
